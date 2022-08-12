e7408b60a0c5e1c433387f01fe43200e373213ab
Bahrii Vadim <bahriivadim@gmail.com>
2022-08-12 11:41:54 +0300

    add stute and move data for component

--- src/App.js
+++ src/App.js
@@ -8,16 +8,15 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-const App = props => {
+const App = ({state:{profilePage,dialogsPage:{messages,dialogs}}}) => {
    return (
-
          <div className="app-wrapper">
             <Header />
             <Navbar />
             <div className="app-wrapper-content">
               <Routes>
-              <Route path="/dialogs" element={<Dialogs  messages={props.messages} dialogs={props.dialogs}  />} />
-              <Route path="/profile" element={<Profile posts={props.posts}/>} />
+              <Route path="/dialogs" element={<Dialogs  messages={messages} dialogs={dialogs}  />} />
+              <Route path="/profile" element={<Profile posts={profilePage.posts}/>} />
 
               <Route path="/news" element={<News/>} />
               <Route path="/music" element={<Music/>} />
--- src/index.js
+++ src/index.js
@@ -3,34 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {BrowserRouter, Router} from "react-router-dom";
+import state from './Redux/state.js'
 
-let posts = [
-   { id: 1, message: 'Hi, how are you?', likesCount: 12 },
-   { id: 2, message: "It's my first post", likesCount: 11 },
-   { id: 3, message: 'Blabla', likesCount: 11 },
-   { id: 4, message: 'Dada', likesCount: 11 },
-];
-let dialogs = [
-   { id: 1, name: 'Dimych' },
-   { id: 2, name: 'Andrew' },
-   { id: 3, name: 'Sveta' },
-   { id: 4, name: 'Sasha' },
-   { id: 5, name: 'Viktor' },
-   { id: 6, name: 'Valera' },
-];
-
-let messages = [
-   { id: 1, message: 'Hi' },
-   { id: 2, message: 'How is your it-kamasutra?' },
-   { id: 3, message: 'Yo' },
-   { id: 4, message: 'Yo' },
-   { id: 5, message: 'Yo' },
-];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
       <BrowserRouter basename="/social-network/">
-      <App posts={posts} dialogs={dialogs} messages={messages}/>
+      <App state={state}/>
          </BrowserRouter>
    </React.StrictMode>,
 );
